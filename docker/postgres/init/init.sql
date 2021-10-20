bdba6474562d4c7ff72b02998c76034cf6503e93
yama0803 <karuwa.reizi@gmail.com>
2021-10-20 20:55:10 +0900

    [update]初期ファイルを修正

--- docker/postgres/init/init.sql
+++ docker/postgres/init/init.sql
@@ -1,2 +1,24 @@
 /* DATABASEを作成 */
 CREATE DATABASE pgtest;
+
+\c pgtest;
+
+CREATE TABLE mybook (
+  id integer, 
+  name varchar(10)
+);
+
+INSERT INTO mybook VALUES (
+    1,
+    'aaaa'
+);
+
+INSERT INTO mybook VALUES (
+    2,
+    'bbbb'
+);
+
+INSERT INTO mybook VALUES (
+    3,
+    'cccc'
+);
